99a2c895ae3e45262b14f029b4cea4fb67a4ce37
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-03-27 23:40:38 +0100

    [UPD] random numbers done

--- js/script.js
+++ js/script.js
@@ -4,14 +4,31 @@ const countdown = document.getElementById('countdown');
 let seconds = 0;
 
 // CORPO DEL PROGRAMMA
-const CountInterval = setInterval(function name() {
-    seconds++
-    console.log(seconds)
-    if (seconds <= 30) {
-        countdown.innerText = seconds;
+// const CountInterval = setInterval(function() {
+//     seconds++
+//     console.log(seconds)
+//     if (seconds <= 30) {
+//         countdown.innerText = seconds;
         
-    }
-    else{
-        clearInterval(CountInterval);
-    }
-}, 1000)
+//     }
+//     else{
+//         clearInterval(CountInterval);
+//     }
+// }, 1000)
+
+// NUMERI CASUALI
+
+let max = 50;
+
+function randomNum(num) {
+    return Math.floor(Math.random() * num) + 1;
+}
+
+const numbersList = document.getElementById('numbers-list')
+
+for (let i = 0; i < 5; i++) {
+    let generateNum = randomNum(max);
+    let list = document.createElement("li")
+     list.textContent = generateNum;
+     numbersList.appendChild(list);
+}
