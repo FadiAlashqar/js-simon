6818f4b3cc4c38213d7258b1f7bd7758c15b7f1c
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-03-27 23:01:02 +0100

    [UPD] countdown done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 // COUNTDOWN
 // richiamo gli elementi su cui andrò a a lvorare:
-const countdown = document.getElementById(countdown);
+const countdown = document.getElementById('countdown');
 let seconds = 0;
+
+// CORPO DEL PROGRAMMA
+const CountInterval = setInterval(function name() {
+    seconds++
+    console.log(seconds)
+    if (seconds <= 30) {
+        countdown.innerText = seconds;
+        
+    }
+    else{
+        clearInterval(CountInterval);
+    }
+}, 1000)
