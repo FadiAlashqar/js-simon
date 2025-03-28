f516ef1a6950e3c32171482f937dc8ef237f74db
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-03-28 02:56:57 +0100

    [UPD] excercise finished

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const CountInterval = setInterval(function() {
     seconds++
     countdown.innerText = seconds;
 
-    if (seconds === 30) {
+    if (seconds === 10) {
         let answersForm = document.getElementById('answers-form')
         let numbersList = document.getElementById('numbers-list')
         
@@ -36,4 +36,50 @@ for (let i = 0; i < 5; i++) {
      list.textContent = generateNum;
      numbersList.appendChild(list);
 }
+
+// CONTROLLO I NUMERI INSERITI E LI CONFROTNO CON QUELLI GENERATI, QUINDI DETERMINO QUANTI NUMERI COMBACIANO
+let answersForm = document.getElementById('answers-form')
+let button = document.querySelector('button')
+
+button.addEventListener('click', function(verify) {
+    verify.preventDefault()
+    
+    let computerNum = numbersList.children;
+    let userNum = answersForm.elements;
+    let correctNums = [];
+
+    for (let j = 0; j < computerNum.length; j++) {
+       
+        let text = computerNum[j].innerText
+
+        for (let m = 0; m < userNum.length; m++) {
+           
+            let value = userNum[m].value
+            
+            if (parseInt(text) === parseInt(value)) {
+                
+                correctNums.push(value);
+                
+                const message = document.getElementById('message')
+                message.innerText = (`hai indovinato ${correctNums.length} numeri :${value++}`)
+            }
+            
+        }   
+        
+    }
+    let output = "";
+    if (correctNums.length > 0) {
+        output = `Hai indovinato ${correctNums.length} numeri: ${correctNums.join(', ')}`;
+    } else {
+        output = "Non hai indovinato nessun numero!";
+    }
+
+    const message = document.getElementById('message');
+    message.innerText = output;
+   
+    
+})
+
+
+
 
